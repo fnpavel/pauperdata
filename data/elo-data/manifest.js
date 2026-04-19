73e351660a00842990ec889c0eca4de80c451836
fnpavel <pnpavel01@gmail.com>
2026-04-19 00:07:21 -0300

    chore(data): import last 1 MTGO events

--- data/elo-data/manifest.js
+++ data/elo-data/manifest.js
@@ -1,7 +1,7 @@
 export const eloManifest = {
-  "generatedAt": "2026-04-18T15:31:29",
-  "lastUpdatedDate": "2026-04-18",
-  "totalMatchCount": 61661,
+  "generatedAt": "2026-04-19T00:07:00",
+  "lastUpdatedDate": "2026-04-19",
+  "totalMatchCount": 61791,
   "years": [
     "2024",
     "2025",
@@ -15,7 +15,7 @@ export const eloManifest = {
   "matchCountsByYear": {
     "2024": 22200,
     "2025": 29852,
-    "2026": 9609
+    "2026": 9739
   },
   "availableDatesByEventType": {
     "online": [
@@ -355,7 +355,8 @@ export const eloManifest = {
       "2026-04-10",
       "2026-04-11",
       "2026-04-16",
-      "2026-04-17"
+      "2026-04-17",
+      "2026-04-18"
     ]
   }
 };
